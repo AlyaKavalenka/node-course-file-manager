5524ed0534201e931bf22e2695a4a724f04cc377
Alya Kovalenko <kovalenkoalinam@gmail.com>
2023-06-24 18:19:12 +0300

    feat: add exit

--- src/index.js
+++ src/index.js
@@ -1,7 +1,43 @@
-function welcome() {
-  const name = process.argv.filter((item) => item.startsWith('--')).join().split('=').slice(1);
+import * as readline from 'node:readline';
 
-  console.log(`Welcome to the File Manager, ${name.length && name[0] ? name : 'Anonymous'}!`);
+const userName = process.argv.filter((item) => item.startsWith('--')).join().split('=').slice(1);
+
+function welcome() {
+  console.log(`Welcome to the File Manager, ${userName.length && userName[0] ? userName : 'Anonymous'}!`);
 }
 
 welcome();
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+  prompt: '> ',
+});
+
+rl.prompt();
+
+const commands = {
+  helpMe() {
+    console.log('I neeeed help! I`m so tired');
+  },
+};
+
+rl
+  .on('line', (input) => {
+    const line = input.trim();
+    const command = commands[line];
+
+    if (line === '.exit') {
+      rl.close();
+    } else if (command) {
+      command();
+    } else {
+      console.log('Unknown command');
+    }
+
+    rl.prompt();
+  })
+  .on('close', () => {
+    console.log(`Thank you for using File Manager, ${userName}, goodbye!`);
+    process.exit();
+  });
