8b48d4930a2e1b25aeb5341e73d3bde19a36cb0d
Alya Kovalenko <kovalenkoalinam@gmail.com>
2023-06-25 13:50:19 +0300

    fix: compress function to extension support

--- src/commands/compressFile.js
+++ src/commands/compressFile.js
@@ -5,8 +5,11 @@ import { failedError } from '../constants.js';
 export default function compressFile(args) {
   const [pathToFile, pathToDestination] = args;
 
+  const isExtension = pathToDestination.split('.')[pathToDestination.split('.').length - 1];
+  const correctPathToDestination = isExtension === 'br' ? pathToDestination : `${pathToDestination}.br`;
+
   const read = fs.createReadStream(pathToFile);
-  const write = fs.createWriteStream(pathToDestination);
+  const write = fs.createWriteStream(correctPathToDestination);
   const brotliZlib = zlib.createBrotliCompress();
 
   read.pipe((brotliZlib)).pipe(write);
