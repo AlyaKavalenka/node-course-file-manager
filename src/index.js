bf6af1f26c2b3cf9a661ad45d3dda5ad0a1745fa
Alya Kovalenko <kovalenkoalinam@gmail.com>
2023-06-26 19:50:18 +0300

    refactor: replace commands into separate js file

--- src/index.js
+++ src/index.js
@@ -1,19 +1,8 @@
 import * as readline from 'node:readline';
 import initialDirectory from './helpers.js';
-import read from './commands/read.js';
-import rename from './commands/rename.js';
-import copy from './commands/copy.js';
-import deleteFile from './commands/deleteFile.js';
-import move from './commands/move.js';
 import cdToFolder from './commands/cdToFolder.js';
-import cdUpper from './commands/cdUpper.js';
-import list from './commands/list.js';
-import createFile from './commands/createFile.js';
-import calcHash from './commands/calcHash.js';
-import getSystemInfo from './commands/getSystemInfo.js';
 import { invalidError } from './constants.js';
-import compressFile from './commands/compressFile.js';
-import decompressFile from './commands/decompressFile.js';
+import commands from './commands.js';
 
 const userName = process.argv.filter((item) => item.startsWith('--')).join().split('=').slice(1);
 
@@ -37,48 +26,6 @@ const rl = readline.createInterface({
 });
 
 rl.prompt();
-
-const commands = {
-  cat(pathToFile) {
-    read(pathToFile);
-  },
-  rn(args) {
-    rename(args);
-  },
-  cp(args) {
-    copy(args);
-  },
-  rm(pathToFile) {
-    deleteFile(pathToFile);
-  },
-  mv(args) {
-    move(args);
-  },
-  cd(pathToDirectory) {
-    cdToFolder(pathToDirectory);
-  },
-  up() {
-    cdUpper();
-  },
-  ls() {
-    list();
-  },
-  add(newFileName) {
-    createFile(newFileName);
-  },
-  hash(pathToFile) {
-    calcHash(pathToFile);
-  },
-  os(flag) {
-    getSystemInfo(flag);
-  },
-  compress(args) {
-    compressFile(args);
-  },
-  decompress(args) {
-    decompressFile(args);
-  },
-};
 
 rl
   .on('line', (input) => {
