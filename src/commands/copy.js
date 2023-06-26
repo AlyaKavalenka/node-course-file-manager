64086009379f5fd361a8a7a876f861a87a7edfc6
Alya Kovalenko <kovalenkoalinam@gmail.com>
2023-06-26 20:11:40 +0300

    refactor: path to new directory

--- src/commands/copy.js
+++ src/commands/copy.js
@@ -1,11 +1,14 @@
 import fs from 'fs';
+import path from 'path';
 import { failedError } from '../constants.js';
 
 export default async function copy(args) {
   const [pathToFile, pathToNewDirectory] = args;
 
+  const correctPathToNewDirectory = pathToNewDirectory.split('.').length > 1 ? pathToNewDirectory : path.join(pathToNewDirectory, pathToFile.split('/')[pathToFile.split('/').length - 1]);
+
   const read = fs.createReadStream(pathToFile);
-  const write = fs.createWriteStream(pathToNewDirectory);
+  const write = fs.createWriteStream(correctPathToNewDirectory);
 
   read.pipe(write);
 
